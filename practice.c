8ea58fd719ff306547c665a687807487b7757f2e
gaurav rai <109055376+gaurav6129@users.noreply.github.com>
2023-03-10 23:53:08 +0530

    changes

--- practice.c
+++ practice.c
@@ -1,43 +1,9 @@
-#include <stdio.h>
+#include "stdio.h"
 int main()
 {
-    int r, c, i, j, k[10][10];
-    printf("enter r=");
-    scanf("%d", &r);
-    printf("enter c=");
-    scanf("%d", &c);
-    printf("Wakanda monuments:-\n");
-    for (i = 0; i < r; i++)
-    {
-        for (j = 0; j < c; j++)
-        {
-            scanf("%d", &k[i][j]);
-        }
-    }
-    printf("path:-\n");
-    for (i = 0; i < r; i++)
-    {
-        for (j = 0; j < c; j++)
-        {
-            printf("%d", k[i][j]);
-        }
-        printf("\n");
-    }
-    printf("traversed by traveller:-\n");
-    for (i = 0; i < c; i++)
-    {
-        if (i % 2 == 0)
-        {
-            printf("%d", k[j][i]);
-        }
-
-        else
-        {
-            for (j = r - 1; j >= 0; j--)
-            {
-                printf("%d", k[j][i]);
-            }
-        }
-    }
-    return 0;
-}
+int x, y = 5, z = 5;
+ x = y == z;
+ printf("%d", x);
+getchar();
+return 0;
+} 
