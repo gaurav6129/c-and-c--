a8f94fd9010e16508ffee71faf5e432897179df4
gaurav rai <109055376+gaurav6129@users.noreply.github.com>
2023-04-21 13:14:49 +0530

    changes

--- first.java
+++ first.java
@@ -1,5 +1,15 @@
-class first{
-    public static void main(string[] args){
-        system.out.println("hello,world");
+import java.util.*;
+
+public class first{
+    public static void main(String args[]){
+
+    
+    //System.out.println("gaurav rai");
+    ArrayList<String>first=new ArrayList<String>();
+    first.add("gaurav");
+    first.add("singh");
+    first.add("rai");
+    System.out.println("ArrayList:" + first);
     }
+
 }
