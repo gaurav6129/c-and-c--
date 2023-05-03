929bea94047cf3318666bf4019ef2ee6954a84a3
gaurav rai <109055376+gaurav6129@users.noreply.github.com>
2023-05-03 08:41:58 +0530

    gaurav

--- first.java
+++ first.java
@@ -1,15 +1,29 @@
+// import java.util.*;
+
+// public class first{
+//     public static void main(String args[]){
+
+//     //System.out.println("gaurav rai");
+//     ArrayList<String>first=new ArrayList<String>();
+//     first.add("gaurav");
+//     first.add("singh");
+//     first.add("rai");
+//     System.out.println("ArrayList:" + first);
+//     }
+
+// }
 import java.util.*;
 
-public class first{
-    public static void main(String args[]){
-
-    
-    //System.out.println("gaurav rai");
-    ArrayList<String>first=new ArrayList<String>();
-    first.add("gaurav");
-    first.add("singh");
-    first.add("rai");
-    System.out.println("ArrayList:" + first);
+public class first {
+    public static void main(String args[])
+    {
+    ArrayList<String>first=new ArrayList<String>(){
+    {
+    add("gaurav");
+    add("kumar");
+    add("rai");
     }
-
+};
+    System.out.println("ArrayList: " +first);
+}
 }
