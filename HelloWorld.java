a7ccd0d47c7dbe00ce9ca1aa03133fa0b531d967
gaurav rai <109055376+gaurav6129@users.noreply.github.com>
2023-08-23 18:46:32 +0530

    commite

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,12 @@
-// Your First Program
+
+import java.util.Scanner;
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("print the number");
+        int num_1 = sc.nextInt();
+        System.out.print("number is:", num_1);
+
     }
 }
